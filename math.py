4b7b718a07a2dfec3664b552cef6f4b039b535f5
Anjali Kushwah <55048057+Anjalikushwah264@users.noreply.github.com>
2022-02-04 15:29:02 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+#Add implimentation
+def add(x,y):
+    return x+y
+
+#subtract implimentation
+def subtract(x,y):
+    return x-y
+
+#multiply implimentation
+def multiply(x,y):
+    return x*y          #on bug456
+
+#divide implimentation
+def divide(x,y):
+    if y==0:
+        return Divide by 0
+    else:
+        return  x/y   
+#squarefun
+def square(a):
+    return a*a
